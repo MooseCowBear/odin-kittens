f92d8cfa50c34d445561738ad9fac1252248f27a
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-05-27 11:17:40 -0400

    seed some kitten data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+cat_names = ["Sneakers", "Cow", "Chip", 
+    "Apple Pie", "Nova", "Comet", "Cayenne", 
+    "Pepper", "Button", "Onyx", "Midnight",
+    "Marshmallow Kibble", "Yum Yum", "Aspen", 
+    "Brulee", "Cannoli", "French Toast", "Isaac", 
+    "Levi", "Maple", "Multi", "Shy"]
+
+cat_names.each do |n|
+  a = rand(0..20)
+  c = rand(1..10)
+  s = rand(1..10)
+  Kitten.create!(name: n, age: a, cuteness: c, softness: s)
+end
