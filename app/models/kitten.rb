5e26c840de02248d1aa6f540a129b90b1c5b0b3f
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-05-28 12:48:27 -0400

    add as_json override

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -2,4 +2,11 @@ class Kitten < ApplicationRecord
   validates :name, presence: true
   validates :age, presence: true,  numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 20 }
   validates :cuteness, :softness, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
+
+  def as_json(_options={})
+      { :name => self.name,
+        :cuteness => self.cuteness,
+        :softness => self.softness
+      }  
+    end
 end
