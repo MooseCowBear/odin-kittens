b2acea98d03798183a53e269c293a2d3b336ab58
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-05-28 12:13:25 -0400

    add custom error pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
     resources :kittens
   end
 
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all 
+  match "/422", to: "errors#unprocessable_content", via: :all 
+
   root "kittens#index"
 end
